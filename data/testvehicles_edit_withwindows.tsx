ea64027db9800a9b5ef3e67777b5bd61bfc6967e
LeDoc <guthub@kunstkram.org>
2020-12-23 21:45:20 +0100

    first test for data

---
+++ data/testvehicles_edit_withwindows.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="testvehicles_edit_withwindows" tilewidth="32" tileheight="32" tilecount="25" columns="5">
+ <image source="../krita/testvehicles_edit_withwindows.png" width="160" height="160"/>
+</tileset>
